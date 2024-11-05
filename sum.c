d0a7e16e448ffcfea6a11074996eeaf037c1b520
Ssing0811 <pnsinghbsb22@gmail.com>
2024-11-05 15:32:14 +0530

    added

---
+++ sum.c
@@ -0,0 +1,10 @@
+//sum of 5 numbers
+#include<stdio.h>
+int main()
+{ float a,b,c,d,e,s;
+ printf("the numbers are");
+ scanf("%f%f%f%f%f",&a,&b,&c,&d,&e);
+ s=a+b+c+d+e;
+ printf("the sum of 5 numbers are %f",s);
+ return 0;
+}
